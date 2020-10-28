858f28bbaa8f273146ec1ec44d458fee2e35e40e
Tomás Díaz Toro <tomas.diaz@cornershopapp.com>
2020-10-28 19:17:31 -0300

    chore: remove redis

--- setup.py
+++ setup.py
@@ -10,9 +10,7 @@ setup(
     long_description=open("README.md").read(),
     install_requires=[
         "Flask==1.1.2",
-        "redis==3.5.3",
         "pytest==6.1.1",
-        "python-redis-lock==3.6.0",
         "requests==2.21.0",
         "six==1.15.0",
         "Flask-Caching==1.8",
